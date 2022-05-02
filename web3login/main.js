e932131af184fb8bfc565bf8a399bbab9c569da6
Caleb Ugenlo <41567741+ugenloc@users.noreply.github.com>
2022-05-03 05:29:46 +0700

    Add files via upload

---
+++ web3login/main.js
@@ -0,0 +1,38 @@
+/* Moralis init code */
+const serverUrl = "https://zyhamndscyf8.usemoralis.com:2053/server";
+const appId = "7Cp0EIGcFW80a0zMJE9wSIumedw1k2XXpfxMBmzG";
+Moralis.start({ serverUrl, appId });
+
+/* Authentication code */
+async function login() {
+  let user = Moralis.User.current();
+  if (!user) {
+    user = await Moralis.authenticate({
+      signingMessage: "Log in using Moralis"
+    })
+      .then(function (user) {
+        console.log("logged in user:", user);
+        let ethAddress = user.get("ethAddress");
+        document.getElementById(
+          "myethAddress"
+        ).textContent = `Logged in Eth Address ${ethAddress}`;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  } else {
+    let ethAddress = user.get("ethAddress");
+    document.getElementById(
+      "myethAddress"
+    ).textContent = `Already Logged in ${ethAddress}`;
+  }
+}
+
+async function logOut() {
+  await Moralis.User.logOut();
+  document.getElementById("myethAddress").textContent = "";
+  console.log("logged out");
+}
+
+document.getElementById("btn-login").onclick = login;
+document.getElementById("btn-logout").onclick = logOut;
